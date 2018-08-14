bcdd2d21e118fb984201f4afad3d2a0934ebc093
Rebeca Miranda <bartromanow@post.com>
2018-08-14 12:08:04 -0400

    improve footer

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -1,9 +1,23 @@
 
   <footer class="footer">
-    <div class="label">Sign up for Karten Newsletter</div>
-    <div class="big-input">
-      <input class="email" placeholder="email" type="email">
-      <img src="<?php echo get_image('right-arrow.png') ?>" alt="k:d" class="right-arrow"></img>
+    <div class="label">Sign up for Karten Design Newsletter</div>
+    <div class="row input-group">
+      <div class="col-sm-6 col-xs-12">
+        <div class="big-input">
+          <input placeholder="First Name">
+        </div>
+      </div>
+      <div class="col-sm-6 col-xs-12">
+        <div class="big-input">
+          <input placeholder="Last Name">
+        </div>
+      </div>
+      <div class="col-xs-12">
+        <div class="big-input">
+          <input class="email" placeholder="Email" type="email">
+          <img src="<?php echo get_image('right-arrow.png') ?>" alt="k:d" class="right-arrow"></img>
+        </div>
+      </div>
     </div>
     <img src="<?php echo get_image('small-logo.png') ?>" alt="k:d" class="footer-logo"></img>
   </footer>
